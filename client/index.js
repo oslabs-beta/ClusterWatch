2d55d48fca71b0d1b82e457fe0bcecabdba3cfbb
Kevin Hendrix <k.hendrix1204@gmail.com>
2023-02-22 16:05:49 -0500

    test-vs

--- client/index.js
+++ client/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('app'),
-);
+const root = createRoot(document.getElementById('mainBody'));
+root.render(<App />);
