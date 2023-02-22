3e8d162828b19154ac443257a1d7bb9755297b7c
mushrathc <mushrath.choudhury@baruchmail.cuny.edu>
2023-02-22 15:09:30 -0500

    implement skeleton

--- server/server.js
+++ server/server.js
@@ -3,6 +3,26 @@ const app = express();
 const PORT = 3000;
 const cors = require('cors');
 
+app.use(express.json());
 app.use(cors());
 
+
+
+// catch all
+app.use((req, res) => res.sendStatus(404));
+
+// global err handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT} `));
+
+module.exports = app;
