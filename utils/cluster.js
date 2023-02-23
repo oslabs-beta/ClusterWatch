5d9a87f4b42eeb1c34dd50ee872952f589046c1c
Kevin Hendrix <k.hendrix1204@gmail.com>
2023-02-23 16:27:16 -0500

    Initialize network visualizer structure

--- utils/cluster.js
+++ utils/cluster.js
@@ -1,18 +1,35 @@
-// const fetchClusterNodes = async () => {
-//   const data = await fetch(
-//     'http://localhost:30000/api/v1/query?query=kube_node_info',
-//     {
-//       method: 'GET',
-//       headers: {
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json',
-//       },
-//     }
-//   ).then((res) => res.json());
-//   const nodes = data.data.result.map((result) => {
-//     return result.metric.node;
-//   });
-//   return nodes;
-// };
+const fetchClusterNodes = async () => {
+  const data = await fetch(
+    'http://localhost:9090/api/v1/query?query=kube_node_info',
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  ).then((res) => res.json());
+  const nodes = data.data.result.map((result) => {
+    return result.metric.node;
+  });
+  console.log(nodes);
+  return nodes;
+};
+const fetchTotalPods = async () => {
+  const data = await fetch(
+    'http://localhost:9090/api/v1/query?query=count(kube_pod_created)',
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  ).then((res) => res.json());
+  const totalPods = data.data.result[0].value[1];
+  console.log(totalPods);
+  return totalPods;
+};
 
-// console.log(await fetchClusterNodes());
+fetchClusterNodes();
+fetchTotalPods();
