eaa43500e08815869685c6df38711ea8f19add4b
Sheng Li <shengli@Shengs-MBP.attlocal.net>
2023-03-01 07:12:30 -0800

    made changes to alerts

--- client/components/dashboard/Banner.jsx
+++ client/components/dashboard/Banner.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Banner({title}) {
-  return (
 
+function Banner({ title }) {
+  return (
     <div id="Banner">
       <h2>{title}</h2>
 
- 
     </div>
   );
 }
--- client/index.js
+++ client/index.js
@@ -1,18 +1,15 @@
-
 import React from 'react';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 import ReactDom, { createRoot } from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const root = createRoot(document.getElementById('mainBody'));
 root.render(
-  <React.StrictMode>
-    <ProSidebarProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ProSidebarProvider>
-  </React.StrictMode>,
+  <ProSidebarProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProSidebarProvider>
 );
